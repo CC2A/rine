39f978ec643409dba02cad140887e5662ec78559
CC2A <Aditrc@outlook.com>
2018-09-04 20:32:55 +0800

    WithOutKey And Minus

--- src/WithOutKey.ts
+++ src/WithOutKey.ts
@@ -1,5 +1,2 @@
-type TheArrKeys<T extends PropertyKey[]> = { [K in (keyof T) extends void ? any : any]: T[K] }
-type GetStrKey<T extends any> = T[string]
-export type Keys<T extends PropertyKey[] | {}> = T extends Array<any> ? GetStrKey<TheArrKeys<T>> : keyof T
-type TheWithOutKey<A, T extends any> = A extends never ? never : { [K in T extends PropertyKey ? T : T]: A[K] }
-export type WithOutKey<A, B extends PropertyKey[]> = TheWithOutKey<A, Exclude<Keys<A>, Keys<B>>>
+export type WithOutKey<A, B extends PropertyKey[]> = Pick<A, Exclude<keyof A, B[any]>>
+export type Minus<A, B> = Pick<A, Exclude<keyof A, keyof B>>
