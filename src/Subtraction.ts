db609e4e4147b7df616474acc9b753620ac47846
CC2A <Aditrc@outlook.com>
2018-09-04 23:37:11 +0800

    Minus fix bug

--- src/Subtraction.ts
+++ src/Subtraction.ts
@@ -3,4 +3,4 @@ export type MinusKey<A, B> = Pick<A, Exclude<keyof A, keyof B>>
 export type KeyExpand<A> = { [P in keyof A]: { [K in P]: A[P] } }
 export type Expand<A> = KeyExpand<A>[keyof KeyExpand<A>]
 export type KeyPack<E> = E extends infer O ? keyof O : any
-export type Minus<A, B> = Pick<A, KeyPack<Exclude<Expand<A>, B>>>
+export type Minus<A, B> = Pick<A, KeyPack<Exclude<Expand<A>, Expand<B>>>>
